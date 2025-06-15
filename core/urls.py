56f1d2093fb9e7b7ca26ea634238700bc0b53135
Natnael Dagnachew <101104482+Nate2784@users.noreply.github.com>
2025-06-15 21:44:02 +0300

    made templates global.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from users.views import login_view
 
 urlpatterns = [
+    path('', login_view, name='login'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
 ]

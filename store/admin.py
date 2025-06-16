f5ff3ff9d2517d6326e44b17b26c317cb0e966c9
Natnael Dagnachew <101104482+Nate2784@users.noreply.github.com>
2025-06-16 21:26:12 +0300

    stashed changes

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *  # or the other user-related model
+
+admin.site.register(Store)
+admin.site.register(StoreManager)
+admin.site.register(StoreCashier)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,37 @@
+<<<<<<< HEAD
+# store/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .forms import StoreCreateForm, StoreManagerAssignForm
+from .models import Store, StoreManager
+
+@login_required
+def create_store(request):
+    if request.user.role != 'store_owner':
+        messages.error(request, "Access denied.")
+        return redirect('store_owner_page')
+
+    if request.method == 'POST':
+        form = StoreCreateForm(request.POST)
+        if form.is_valid():
+            store = form.save(commit=False)
+            store.owner = request.user  # <-- set the owner
+            store.save()
+            messages.success(request, f"Store '{store.name}' created successfully.")
+            return redirect('store_owner_page')  # or wherever you want
+    else:
+        form = StoreCreateForm()
+
+    return render(request, 'store/create_store.html', {'form': form})
+
+
+
+
+
+
+=======
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
@@ -31,3 +65,4 @@ class StoreDeleteView(LoginRequiredMixin, StoreOwnerRequiredMixin, DeleteView):
     model = Store
     template_name = 'store/store_confirm_delete.html'
     success_url = reverse_lazy('store_list')
+>>>>>>> 47f5317cd9a21712765f9056ed714eab714307ef

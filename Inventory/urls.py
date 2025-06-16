fd3092579a14e31e5b53e6d2b13dddfcc6a7f685
XBennyX2 <ezeraben47@gmail.com>
2025-06-16 18:57:25 +0300

    added urls for inventory

---
+++ Inventory/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView,
+    ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView,
+    StockListView
+)
+
+urlpatterns = [
+    path('categories/', CategoryListView.as_view(), name='category_list'),
+    path('categories/new/', CategoryCreateView.as_view(), name='category_create'),
+    path('categories/<int:pk>/edit/', CategoryUpdateView.as_view(), name='category_update'),
+    path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category_delete'),
+    path('products/', ProductListView.as_view(), name='product_list'),
+    path('products/new/', ProductCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+    path('stock/', StockListView.as_view(), name='stock_list'),
+]
